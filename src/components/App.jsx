b803813e3877f7db2ca47d6494d5d277d4cf8c74
28Rostyk <107403659+28Rostyk@users.noreply.github.com>
2023-06-01 16:48:44 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,17 @@
 import { Route, Routes } from 'react-router-dom';
 import { Navigate } from 'react-router-dom';
 import SharedLayout from './SharedLayout/SharedLayout';
-import ShopPage from 'pages/ShopPage';
-import ShoppingCartPage from 'pages/ShoppingCartPage';
-import CoupontsPage from 'pages/CouponsPage/CouponsPage';
-import HistoryPage from 'pages/HistoryPage/HistoryPage';
-import RegisterPage from 'pages/RegisterPage';
-import LoginPage from 'pages/LoginPage';
+import ShopPage from '../pages/ShopPage';
+import ShoppingCartPage from '../pages/ShoppingCartPage';
+import CoupontsPage from '../pages/CouponsPage/CouponsPage';
+import HistoryPage from '../pages/HistoryPage/HistoryPage';
+import RegisterPage from '../pages/RegisterPage';
+import LoginPage from '../pages/LoginPage';
 export const App = () => {
   return (
     <Routes>
       <Route path="/" element={<SharedLayout />}>
-        <Route path="/MyShopsFront" element={<Navigate to="/shop" replace />} />
+        <Route path="MyShopsFront" element={<Navigate to="/shop" replace />} />
         <Route path="/shop" element={<ShopPage />} />
         <Route path="/shopping" element={<ShoppingCartPage />} />
         <Route path="/history" element={<HistoryPage />} />
